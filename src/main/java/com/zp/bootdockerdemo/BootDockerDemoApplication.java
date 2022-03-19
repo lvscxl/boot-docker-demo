4a05b68dd6445b91ac877be3e35ffbda3995c7b0
lvshancun <lvscxl@sina.com>
2022-03-19 16:42:34 +0800

    demo

---
+++ src/main/java/com/zp/bootdockerdemo/BootDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zp.bootdockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDockerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootDockerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zp/bootdockerdemo/BootDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zp.bootdockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDockerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
